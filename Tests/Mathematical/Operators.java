cbb828638290911fc5b784c2c3e7aa50c0fa2aa5
JemmaFromJupiter <starecki.j@shaw.ca>
2024-01-05 12:01:04 -0700

    Finished 'Tests\Mathematical\Operators.java'

--- Tests/Mathematical/Operators.java
+++ Tests/Mathematical/Operators.java
@@ -1,9 +1,15 @@
-package Tests.Mathematical;
-
 public class Operators {
     public static void main(String[] args) {
         int x = 50;
         int y = 10;
-        int z = x + y;
+        int add = x + y;
+        int sub = x - y;
+        int mul = x * y;
+        float div = x / y;
+        int mod = x % y;
+        int incr = ++x;
+        int deincr = --y;
+
+        System.out.printf("Add: %d\nSub: %d\nMul: %d\nDiv: %f\nMod: %d\nIncr: %d\nDeincr: %d", add, sub, mul, div, mod, x, y);
     }
 }
