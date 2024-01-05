c00812d1ae2857ece48202d4d8753f45da8b844d
JemmaFromJupiter <starecki.j@shaw.ca>
2024-01-05 12:34:25 -0700

    Finished 'Tests\Mathematical\VariableAssignment.java'

--- Tests/Mathematical/VariableAssignment.java
+++ Tests/Mathematical/VariableAssignment.java
@@ -1,5 +1,16 @@
 public class VariableAssignment {
     public static void main(String[] args) {
-        
+        int x = 10;
+
+        x += 5; // Add to x. Equivalent to x = x + 5
+        System.out.println(x);
+        x -= 5; // Subtract from x. Equivalent to x = x - 5
+        System.out.println(x);
+        x *= 5; // Multiplying x. Equivalent to x = x * 5;
+        System.out.println(x);
+        x /= 5; // Dividing x. Equivalent to x = x / 5
+        System.out.println(x);
+        x %= 5; // Remainder of x. Equivalent to x = x % 5
+        System.out.println(x);
     }
 }
