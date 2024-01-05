a7bc71b8d8de4eedd4990c318d48107d4c02f878
JemmaFromJupiter <starecki.j@shaw.ca>
2024-01-05 11:52:20 -0700

    Testing Auto Push + Added to 'Tests\Mathematical\Operators.java'

--- Tests/Mathematical/Operators.java
+++ Tests/Mathematical/Operators.java
@@ -2,6 +2,8 @@ package Tests.Mathematical;
 
 public class Operators {
     public static void main(String[] args) {
-        
+        int x = 50;
+        int y = 10;
+        int z = x + y;
     }
 }
