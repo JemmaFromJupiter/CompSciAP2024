2518b251ecd5c49e283d4d0268d05ae81d10191e
JemmaFromJupiter <starecki.j@shaw.ca>
2024-01-05 14:00:02 -0700

    Testing Auto Push

--- Module 1/Assignment2/Main.java
+++ Module 1/Assignment2/Main.java
@@ -2,20 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
 
-class myClass {
-    int x;
-}
-
 class Main {
     public static void main(String[] args) {
     // <== Global variables, Used all throughout the program ==> //
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        myClass newMyClass = new myClass();
-
-        newMyClass.x = 17;
-
-        System.out.println(newMyClass.x);
     }
 
     public static void clear() {
