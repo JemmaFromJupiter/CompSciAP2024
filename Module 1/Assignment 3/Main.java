5a3fcd3d0ee1a8b0039dafd52f1d446fd4347411
JemmaFromJupiter <starecki.j@shaw.ca>
2024-01-08 13:20:51 -0700

    Added Assignment 3

---
+++ Module 1/Assignment 3/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner input;
+    public static void main(String[] args) {
+        input = new Scanner(System.in);
+        
+        int userInt = get_number("Input an integer: ", 1, 3);
+        float userFloat = get_number("Input a float:", 1.0f, 3.0f);
+        boolean userBool = confirm("Confirm?");
+
+        System.out.printf("The Input Integer: %d\n", userInt);
+        System.out.printf("The Input Float: %f\n", userFloat);
+        System.out.printf("The Input Confirmation: %b\n", userBool);
+    }
+
+    public static int get_number(String message, int min, int max) {
+        int userNum = 0;
+
+        do {
+            System.out.println(message);
+            if (input.hasNextInt()) {
+                userNum = input.nextInt();
+                if (min <= userNum && userNum <= max) {
+                    input.nextLine();
+
+                    return userNum;
+                } else {
+                    System.out.printf("Input must be within the range %d - %d.\n", min, max);
+                }
+            } else {
+                input.nextLine();
+                System.out.println("Input Must Be An Integer.");
+
+                continue;
+            }
+        } while (true);
+    }
+
+    public static float get_number(String message, float min, float max) {
+        float userNum = 0.0f;
+
+        do {
+            System.out.println(message);
+            if (input.hasNextFloat()) {
+                userNum = input.nextFloat();
+                if (min <= userNum && userNum <= max) {
+                    input.nextLine();
+
+                    return userNum;
+                } else {
+                    System.out.printf("Input must be within the range %f - %f.\n", min, max);
+                }
+            } else {
+                input.nextLine();
+                System.out.println("Input Must Be A Float.");
+
+                continue;
+            }
+        } while (true);
+    }
+
+    public static boolean confirm(String message) {
+        String userInput = "";
+
+        do {
+            System.out.println(message);
+            if (input.hasNext()) {
+                userInput = input.nextLine();
+                if (userInput.equals("Y")) {
+                    return true;
+                } else if (userInput.equals("N")) {
+                    return false;
+                } else {
+
+                    System.out.println("Input must be Y or N.");
+                }
+            } else {
+                input.nextLine();
+
+                System.out.println("Input must not be empty.");
+            }
+        } while (true);
+    }
+}
