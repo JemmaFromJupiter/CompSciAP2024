cb7bcdd0e3c4274ae9683a35bdabcbe098d73a72
JemmaFromJupiter <starecki.j@shaw.ca>
2024-02-01 11:41:24 -0700

    Created Temp Package to make it so I dont have to have the LinkedList class declared in EVERY file

---
+++ Module2/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,224 @@
+package Module2.LinkedList;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public DoublyLinkedList(int head) {
+        Node newNode = new Node(head);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    public DoublyLinkedList(int[] a) {
+        this();
+        for (int n : a) {
+            this.append(n);
+        }
+    }
+
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data, Node prev, Node next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public Node getNode(int index) {
+        Node currentNode = head;
+        for (int currentIndex = 0; currentIndex < index; currentIndex++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+
+    public int get(int idx) {
+        return this.getNode(idx).data;
+    }
+
+    public void set(int idx, int data) {
+        Node node = this.getNode(idx);
+        node.data = data;
+    }
+
+    public void insert(int newData) {
+        Node newNode = new Node(newData);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            length += 1;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+
+        head = newNode;
+        length += 1;
+    }
+
+    public void insert(int index, int newData) {
+
+        Node newNode = new Node(newData);
+        Node prevNode = this.getNode(index-1);
+        Node nextNode = prevNode.next;
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            length += 1;
+            return;
+        }
+
+        prevNode.next = newNode;
+        newNode.next = nextNode;
+        newNode.prev = prevNode;
+        nextNode.prev = newNode;
+
+        length += 1;
+    }
+
+    public void append(int newData) {
+        Node newNode = new Node(newData);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            length += 1;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+
+        length += 1;
+
+    }
+
+    public void append(DoublyLinkedList l) {
+        Node last = l.head;
+        last.prev = this.tail;
+        this.tail.next = last;
+        this.tail = l.tail;
+
+        length += l.getLength();
+    }
+
+    public void delete(int index) {
+        if (index == 0) {
+            head = head.next;
+            head.prev = null;
+
+            length -= 1;
+            return;
+        } else if (index == length-1) {
+            tail = tail.prev;
+            tail.next = null;
+
+            length -= 1;
+            return;
+        } else {
+            Node node = this.getNode(index);
+
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+
+            length -= 1;
+            return;
+            
+        }
+    }
+
+    public int pop(int idx) {
+        int nodeValue = this.getNode(idx).data;
+        this.delete(idx);
+        return  nodeValue;
+    }
+
+    public int pop() {
+        return this.pop(length-1);
+    }
+
+    public int find(int val) {
+        int idx;
+
+        for (idx = 0; idx < length; idx++) {
+            if (this.getNode(idx).data == val) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+
+    public void swap(int idx1, int idx2) {
+        Node node1 = this.getNode(idx1);
+        Node node2 = this.getNode(idx2);
+        Node tempPrev = node1.prev;
+        Node tempNext = node1.next;
+
+        node1.prev = node2.prev;
+        node1.next = node2.next;
+
+        node2.prev = tempPrev;
+        node2.next = tempNext;
+
+        node2.prev.next = node2;
+        node2.next.prev = node2;
+
+        node1.prev.next = node1;
+        node1.next.prev = node1;
+    }
+
+    public void swapByObject(int obj1, int obj2) {
+        int idx1 = this.find(obj1);
+        int idx2 = this.find(obj2);
+
+        swap(idx1, idx2);
+    }
+
+    public String toString() {
+        StringBuilder returnString = new StringBuilder();
+        Node last = head;
+        while (last != null) {
+            if (last.prev != null) {
+                returnString.append("<- ");
+            } else {
+                returnString.append("Head :: ");
+            }
+            returnString.append(last.data);
+            if (last.next != null) {
+                returnString.append(" ->");
+            } else {
+                returnString.append(" :: Tail");
+            }
+            last = last.next;
+        }
+        return returnString.toString();
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
