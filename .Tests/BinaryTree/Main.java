ffdcd0c1915dcb9e7d82ed2c0a00f9ea48e39872
JemmaFromJupiter <starecki.j@shaw.ca>
2024-02-05 17:34:57 -0700

    Very Bad Implementation of a Binary Tree

--- .Tests/BinaryTree/Main.java
+++ .Tests/BinaryTree/Main.java
@@ -1,14 +1,17 @@
+import java.util.Random;
 
 public class Main {
+  private static Random random = new Random();
+
+  public static int[] generateRandomArray(int max, int size) {
+    return random.ints(size, 0, max).toArray();
+  }
+
   public static void main(String[] args) {
-    BinaryTree bt = new BinaryTree();
-    bt.insert(10);
-    bt.insert(5);
-    bt.insert(15);
-    bt.insert(20);
-    bt.insert(0);
+    BinaryTree bt = new BinaryTree(new int[] { 5, 4, 3, 2, 1, 6, 7, 8 });
 
     System.out.println(bt);
+    System.out.println(bt.search(7));
   }
 }
 
@@ -23,10 +26,16 @@ class BinaryTree {
     this.root = new Node(root);
   }
 
+  public BinaryTree(int[] data) {
+    this.root = new Node(data[0]);
+    for (int i = 1; i < data.length; i++) {
+      this.insert(data[i]);
+    }
+  }
+
   class Node {
     int data;
-    private Node left;
-    private Node right;
+    private Node left, right;
 
     public Node(int data, Node left, Node right) {
       this.data = data;
@@ -56,8 +65,54 @@ class BinaryTree {
       }
     }
 
+    public void insert(BinaryTree bt) {
+      this.insert(bt.root);
+    }
+
+    public void inOrder() {
+      if (this.left != null) {
+        this.left.inOrder();
+      }
+      System.out.printf("%d, ", this.data);
+      if (this.right != null) {
+        this.right.inOrder();
+      }
+    }
+
+    public void reverseOrder() {
+      if (this.right != null) {
+        this.right.reverseOrder();
+      }
+      System.out.printf("%d, ", this.data);
+      if (this.left != null) {
+        this.left.reverseOrder();
+      }
+    }
+
+    public int search(int value) {
+      int idx = 0;
+
+      if (value < this.data) {
+        idx += this.left.search(value);
+      }
+
+      if (value > this.data) {
+        idx += this.right.search(value);
+      }
+
+      if (value == this.data) {
+        return idx + 1;
+      }
+
+      if (idx == 0) {
+        idx = -1;
+      }
+
+      return idx;
+    }
+
     public String toString() {
-      return String.format("[%s Right <- %d -> Left %s]", this.left, this.data, this.right);
+      return String.format("[%s Left <- %d -> Right %s]", this.left, this.data, this.right);
     }
   }
 
@@ -71,6 +126,35 @@ class BinaryTree {
     }
 
     this.root.insert(newNode);
+  }
+
+  public void insert(BinaryTree bt) {
+    if (this.root == null) {
+      this.root = bt.root;
+      return;
+    }
+
+    this.root.insert(bt);
+  }
+
+  public void inOrder() {
+    if (this.root == null) {
+      return;
+    }
+    this.root.inOrder();
+    System.out.println();
+  }
+
+  public void reverseOrder() {
+    if (this.root == null) {
+      return;
+    }
+    this.root.reverseOrder();
+    System.out.println();
+  }
+
+  public int search(int value) {
+    return this.root.search(value);
   }
 
   public String toString() {
