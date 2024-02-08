19284a5635ede6f51e2abff2f2b9b1b43d99e184
JemmaFromJupiter <starecki.j@shaw.ca>
2024-02-08 09:15:21 -0700

    Finished Iterable Stack Objejct

--- Module3/Assignment1/Main.java
+++ Module3/Assignment1/Main.java
@@ -1,16 +1,42 @@
+import java.util.Iterator;
 
 public class Main {
   public static void main(String[] args) {
-    Stack<Runnable> stack = new Stack<Runnable>();
-    stack.push(new Runnable() {
-      public void run() {
-        System.out.println("Hello World!");
-      }
-    });
+    Stack<String> stack = new Stack<String>(new String[] { "Hello", "World", "!" });
+    Stack<String> stack_ = new Stack<String>(new String[] { "This", "is", "a", "Stack" });
+    for (String i : stack) {
+      System.out.println(i);
+    }
+    stack.push(stack_);
+    for (String i : stack) {
+      System.out.println(i);
+    }
   }
 }
 
-class Stack<T> {
+class Stack<T> implements Iterable<T> {
+  private class StackIterator implements Iterator<T> {
+    Node next;
+
+    public StackIterator(Stack<T> stack) {
+      this.next = stack.topLevel;
+    }
+
+    public boolean hasNext() {
+      return (this.next != null);
+    }
+
+    public T next() {
+      T temp = this.next.get();
+      this.next = this.next.getNext();
+      return temp;
+    }
+  }
+
+  public Iterator<T> iterator() {
+    return new StackIterator(this);
+  }
+
   private Node topLevel;
   private int size;
 
@@ -61,8 +87,22 @@ class Stack<T> {
     }
 
     public String toString() {
-      return this.data.toString();
+      if (this.next == null) {
+        return this.data.toString();
+      }
+      return String.format("%s -> %s", this.data.toString(), this.next);
     }
+
+  }
+
+  private Node walkToBottom() {
+    Node current = this.topLevel;
+
+    while (current.getNext() != null) {
+      current = current.getNext();
+    }
+
+    return current;
   }
 
   public void push(T data) {
@@ -79,14 +119,22 @@ class Stack<T> {
     this.size += 1;
   }
 
+  public void push(Stack<T> stack) {
+    Node bottom = stack.walkToBottom();
+
+    bottom.setNext(this.topLevel);
+    this.topLevel = stack.topLevel;
+    this.size += stack.size();
+  }
+
   public T pop() {
     T tempData = this.topLevel.data;
-    this.topLevel = this.topLevel.next;
+    this.topLevel = this.topLevel.getNext();
     return tempData;
   }
 
-  public Node peek() {
-    return this.topLevel;
+  public T peek() {
+    return this.topLevel.data;
   }
 
   public boolean isEmpty() {
@@ -97,12 +145,20 @@ class Stack<T> {
     return this.size;
   }
 
-  public String toString() {
-    return "";
+  public boolean has(T val) {
+    for (T item : this) {
+      if (item == val) {
+        return true;
+      }
+    }
+    return false;
   }
 
-}
-
-class FunctionStack<Runnable> implements Stack<Runnable> {
+  public String toString() {
+    if (this.isEmpty()) {
+      return "Stack Is Empty.";
+    }
+    return this.topLevel.toString();
+  }
 
 }
